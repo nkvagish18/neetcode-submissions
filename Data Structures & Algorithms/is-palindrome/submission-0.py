a13f57583c04c51eeeb307fa24b9f7c9d12040aa
nkvagish18 <nkvagish24aiml@rnsit.ac.in>
2026-09-10 20:47:20 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        ans=""
+        for ch in s:
+            if ch.isalnum():
+                ans += ch.lower()
+        return ans == ans[::-1]
